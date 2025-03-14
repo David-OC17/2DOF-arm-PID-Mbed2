e9fb7b97ae47f7be7ff8ce9eaac3a6cb248bea6f
David-OC17 <daveoc01@icloud.com>
2025-03-14 11:49:57 -0600

    this shi working

--- src/main.c
+++ src/main.c
@@ -6,10 +6,12 @@
 void setup();
 void loop();
 
-const float setpoint = 90;
-// static const float Kp = 5, Ki = 0.11, Kd = 0.3;
-static const float Kp = 1.7, Ki = 0.8, Kd = 0.1;
-float integral = 0.0;
+const float setpoint1 = 90;
+const float setpoint2 = 90;
+static const float Kp1 = 5, Ki1 = 0.11, Kd1 = 0.3;
+static const float Kp2 = 1.7, Ki2 = 0.8, Kd2 = 0.1;
+float integral1 = 0.0;
+float integral2 = 0.0;
 uint64_t lastTime = 0;
 
 // Anti-windup Integral Limits
@@ -23,32 +25,7 @@ float constrain(float num, float lower, float upper) {
   return (num < lower) ? lower : (num > upper) ? upper : num;
 }
 
-void update_PID() {
-  // float Kp = 1.7, Ki = 0.8, Kd = 0.001;
-  // float Kp = 2.3, Ki = 1.3, Kd = 0.015;
-  uint64_t now = get_absolute_time();
-  float deltaTime =
-      (float)(now - lastTime) / 1000000.0f; // Ensure float division
-  lastTime = now;
-
-  float error = setpoint - _approximate_joint_state.joint1_theta;
-
-  integral += error * deltaTime;
-  integral = constrain(integral, integralMin, integralMax);
-
-  float output =
-      Kp * error + Ki * integral + Kd * _approximate_joint_state.joint1_omega;
-
-  _control_law_msg.data.data[0] = constrain(output, LOWER_LIMIT, UPPER_LIMIT);
-}
-
 int main() {
-  // TEST_pwm_output();
-  // TEST_control_motor();
-  // TEST_encoder_count_print();
-  // TEST_voltage_control_ros_subscribe();
-  // TEST_kf_generic_update();
-
   setup();
   loop();
 
@@ -76,98 +53,56 @@ void setup() {
   control_motor1(0);
   control_motor2(0);
 
-  // rmw_uros_set_custom_transport(
-  //     true, NULL, pico_serial_transport_open, pico_serial_transport_close,
-  //     pico_serial_transport_write, pico_serial_transport_read);
-
-  // rcl_ret_t ret =
-  //     rmw_uros_ping_agent(REACH_AGENT_TIMEOUT_MS, REACH_AGENT_MAX_ATTEMPTS);
-  // if (ret != RCL_RET_OK) {
-  //   // Unreachable agent
-  //   error_loop();
-  // }
-
   init_encoder_interrupt();
-
-  // init_ros_nodes();
 
   // Set to default values
   init_joint_state_values();
 
   // Init Kalman filter and configure first values
   init_joint_state_kalman(ENCODER1_INITAL_POS_DEG, ENCODER2_INITAL_POS_DEG);
-
-  // TODO change calibration period to being determined by continuity in a GPIO,
-  // then start running
-  // calibrate_joint_state_kalman(200);
 }
 
 void loop() {
-  // while (1) {
-  //   update_PID();
-
-  //   control_motor1(_control_law_msg.data.data[0]);
-
-  //   joint_state_callback();
-  // }
-
   while (1) {
     uint64_t now = get_absolute_time();
     float deltaTime =
-        (float)(now - lastTime) / 1000000.0f; // Ensure float division
+        (float)(now - lastTime) / 1000000.0f;
     lastTime = now;
 
-    float error = setpoint - _approximate_joint_state.joint2_theta/-2.2;
-    // printf("SP: %f, ERROR: %f, THETA: %f", setpoint, error, _approximate_joint_state.joint2_theta/360*470*2);
-    printf("error: %f", error/-2.2);
-    integral += error * deltaTime;
-    integral = constrain(integral, integralMin, integralMax);
+    float error1 = setpoint1 - _approximate_joint_state.joint1_theta/2.2;
+    float error2 = setpoint2 - _approximate_joint_state.joint2_theta/-2.2;
+    
+    integral1 += error1 * deltaTime;
+    integral2 += error2 * deltaTime;
+    integral1 = constrain(integral1, integralMin, integralMax);
+    integral2 = constrain(integral2, integralMin, integralMax);
 
-    float output =
-        Kp * error + Ki * integral + Kd * _approximate_joint_state.joint2_omega;
+    float output1 =
+        Kp1 * error1 + Ki1 * integral1 + Kd1 * _approximate_joint_state.joint1_omega;
 
-    _control_law_msg.data.data[1] = constrain(output, LOWER_LIMIT, UPPER_LIMIT);
+    float output2 =
+        Kp2 * error2 + Ki2 * integral2 + Kd2 * _approximate_joint_state.joint2_omega;
 
-    control_motor2(_control_law_msg.data.data[1]);
+    output1 = constrain(output1, LOWER_LIMIT, UPPER_LIMIT);
+    output2 = constrain(output2, LOWER_LIMIT, UPPER_LIMIT);
+    
+    control_motor2(output2);
+    control_motor1(output1);
 
     _measured_joint_state = take_measurement_encoders();
 
+    KF_Update(&_joint1_kalman_filter, _control_law_msg.data.data[0],
+              _measured_joint_state.joint1_theta);
     KF_Update(&_joint2_kalman_filter, _control_law_msg.data.data[1],
               _measured_joint_state.joint2_theta);
 
     _approximate_joint_state.joint2_theta =
         KF_get_pos_estimate(&_joint2_kalman_filter);
+    _approximate_joint_state.joint1_theta =
+        KF_get_pos_estimate(&_joint1_kalman_filter);
     _approximate_joint_state.joint2_omega =
         KF_get_vel_estimate(&_joint2_kalman_filter);
+    _approximate_joint_state.joint1_omega =
+        KF_get_vel_estimate(&_joint1_kalman_filter);
   }
-
-  // while (1) {
-  //   uint64_t now = get_absolute_time();
-  //   float deltaTime =
-  //       (float)(now - lastTime) / 1000000.0f; // Ensure float division
-  //   lastTime = now;
-
-  //   float error = setpoint - _approximate_joint_state.joint1_theta/2.2;
-  //   printf("ERROR: %f, THETA: %f", error, _approximate_joint_state.joint1_theta);
-
-  //   integral += error * deltaTime;
-  //   integral = constrain(integral, integralMin, integralMax);
-
-  //   float output =
-  //       Kp * error + Ki * integral + Kd * _approximate_joint_state.joint1_omega;
-
-  //   _control_law_msg.data.data[0] = constrain(output, LOWER_LIMIT, UPPER_LIMIT);
-
-  //   control_motor1(_control_law_msg.data.data[0]);
-
-  //   _measured_joint_state = take_measurement_encoders();
-
-  //   KF_Update(&_joint1_kalman_filter, _control_law_msg.data.data[0],
-  //             _measured_joint_state.joint1_theta);
-
-  //   _approximate_joint_state.joint1_theta =
-  //       KF_get_pos_estimate(&_joint1_kalman_filter);
-  //   _approximate_joint_state.joint1_omega =
-  //       KF_get_vel_estimate(&_joint1_kalman_filter);
-  // }
 }
